091704b4140e517848b5028300ae8e9b8cc9b26a
fuerchter <fuerchter@gmail.com>
2013-07-10 07:30:22 +0200

    Create Game::gameTiles

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+	bool[,] gameTiles;
+	void Start () {
+		gameTiles=new bool[18, 10];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
